5eb69c7d82bd84041fbde328cf42815e266551b1
Ahmed Sobih <163659912+a7medsobih@users.noreply.github.com>
2025-12-23 17:05:36 +0200

    to update

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -3,20 +3,20 @@
 import { useSession } from "next-auth/react";
 
 export default function Dashboard() {
-  const { data: session, status } = useSession();
+  // const { data: session, status } = useSession();
 
-  if (status === "loading") {
-    return (
-      <div className="min-h-screen flex items-center justify-center">
-        <h1 className="text-2xl font-semibold">Loading...</h1>
-      </div>
-    );
-  }
+  // if (status === "loading") {
+  //   return (
+  //     <div className="min-h-screen flex items-center justify-center">
+  //       <h1 className="text-2xl font-semibold">Loading...</h1>
+  //     </div>
+  //   );
+  // }
 
   return (
     <div className="min-h-screen flex items-center justify-center">
       <h1 className="text-2xl font-bold">
-        Welcome, {session?.user?.name || "Guest"}
+        Welcome,
       </h1>
     </div>
   );
